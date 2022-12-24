d48443a99c2aca236f415be4bb20d5afce761d00
Antoine Revel <antoine.revel@gmail.com>
2022-12-24 16:52:35 -0500

    candel

--- main.py
+++ main.py
@@ -66,12 +66,49 @@ def menuBidAsk():
             print("Invalid choice. Please try again.")
 
 
+def get_order_book(asset):
+    # Send a GET request to the Coinbase Pro API
+    response = requests.get(f"https://api.pro.coinbase.com/products/{asset}/book")
+
+    # Check the status code to make sure the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        # The order book is stored in the "bids" and "asks" fields of the response
+        bids = data['bids']
+        asks = data['asks']
+        # Return the order book
+        return bids, asks
+    else:
+        # Print an error message if the request was unsuccessful
+        print("Error:", response.status_code)
+
+import requests
+
+def get_candlestick_data(asset, granularity):
+    # Send a GET request to the Coinbase Pro API
+    response = requests.get(f"https://api.pro.coinbase.com/products/{asset}/candles", params={'granularity': granularity})
+
+    # Check the status code to make sure the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        # The candlestick data is returned as a list of lists
+        candles = data
+        # Return the candlestick data
+        return candles
+    else:
+        # Print an error message if the request was unsuccessful
+        print("Error:", response.status_code)
+
+
+
 
 
 
 def print_menu():
   print("1. Get all pairs listed on coin base")
   print("2. Get the bid or ask on a pair")
+  print("3. Get the orderbook on a pair")
+  print("4. Get candel")
 def menu():
     while True:
         print_menu()
@@ -81,5 +118,17 @@ def menu():
             GetCoin()
         if choice =="2":
             menuBidAsk()
+        if choice== "3":
+            asset = input("Enter the asset (e.g., BTC-USD): ")
+            print(get_order_book(asset))
+        if choice =="4":
+            asset = input("Enter the asset (e.g., BTC-USD): ")
+            granularity = input("Enter the granularity (in seconds): ")
+            # Convert the granularity to an integer
+            granularity = int(granularity)
+            print(get_candlestick_data(asset, granularity))
+
+
+
 
 menu()
